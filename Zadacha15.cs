34bc3796c7e5c38b81f8c202092555e221e406ae
DmitryMazaev <118983899+DmitryMazaev@users.noreply.github.com>
2022-12-04 12:31:40 +0300

    Update Zadacha15.cs

--- Zadacha15.cs
+++ Zadacha15.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+{while (a>7)
+    {
+        a = a - 7;
+    }
+    if (a>=1 && a<=5)
+    {
+         Console.WriteLine("Является ли данный день выходным? Нет");
+    }
+    else if (a>=6 && a<=7)
+    {
+        Console.WriteLine("Является ли данный день выходным? Да");
+    }
+}
